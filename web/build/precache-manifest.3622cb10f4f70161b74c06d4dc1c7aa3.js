b216c05f1a38232da1ca7b3010601b7447c8ceab
Abhishek Dixit <abhishek.dixit@genisys-group.com>
2019-04-06 15:08:24 +0530

    Added Build for App

---
+++ web/build/precache-manifest.3622cb10f4f70161b74c06d4dc1c7aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6e8a1c88bb237d46861",
+    "url": "/static/js/main.116f4f3b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e6e8a1c88bb237d46861",
+    "url": "/static/css/main.56984d77.chunk.css"
+  },
+  {
+    "revision": "af7fbd5fb4f4585bfe0ff005e01162d3",
+    "url": "/index.html"
+  }
+];
